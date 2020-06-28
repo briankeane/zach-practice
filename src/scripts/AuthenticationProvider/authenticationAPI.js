c85804ab6913de671f426a2f2926bf0c7358e3ef
Brian Keane <lonesomewhistle@gmail.com>
2020-06-28 10:22:54 -0500

    get buttons working

---
+++ src/scripts/AuthenticationProvider/authenticationAPI.js
@@ -0,0 +1,82 @@
+//Setting url will be used for all fetch calls
+const url = "http://localhost:3000/users";
+
+
+// These will be altered to become our fetch calls when we are ready for them
+let nextID = 3;
+const allUsers = [
+	{
+		id: 1,
+		username: 'bob',
+		email: 'bob@bob.com',
+		password: 'bobspassword'
+	},
+	{
+		id: 2,
+		username: 'Sue',
+		email: 'sue@sue.com',
+		password: 'suespassword'
+	}
+];
+
+
+const authenticationAPI = {
+	getAllUsers() {
+		// return fetch(`${url}/users`)
+		// 	.then(response => response.json());
+
+		return new Promise(function (resolve, reject) {
+			return resolve(allUsers);
+		});
+	},
+	createUser(username, email, password) {
+		// return fetch(`${url}/users`,
+		// 	method: 'POST',
+		// 	headers: { 'Content-Type': 'application/json' },
+		// 	body: JSON.stringify({ username: username, email: email, password: password })
+		// )
+		// 	.then(response => response.json());
+		return new Promise(function (resolve, reject) {
+			let newUser = { 
+				id: nextID++,
+				username: username, 
+				password: password,
+			 	email: email
+			};
+			allUsers.push(newUser);
+			return resolve(newUser);
+		});
+	}
+}
+
+
+
+
+
+
+// url.forEach(user => {
+
+//     for (const value of Object.values(user)) {
+//       console.log(value)
+//     }
+// })
+
+
+// const auththenticationAPI = {
+//     getSingleUser: (id) => {
+//         return fetch(`${url}/users/${id}`)
+//         .then(response => response.json());
+//     },
+
+//     addAUser: (userObj) => {
+// 		return fetch( {
+// 			method: "POST",
+// 			headers: {
+// 				"Content-Type": "application/json"
+// 			},
+// 			body: JSON.stringify(userObj)
+// 		}).then(response => response.json());
+//     }   
+// }
+
+export default authenticationAPI;
--- src/scripts/AuthenticationProvider/authenticationMain.js
+++ src/scripts/AuthenticationProvider/authenticationMain.js
@@ -1,2 +1,79 @@
-import blankObject from "./authenticationClickEvents.js";
+import api from "./authenticationAPI.js"
+
+const authenticationSubmitButton = document.querySelector('#authentication-submit-button');
+const createAccountSubmitButton = document.querySelector('#create-account-submit-button');
+const signoutSubmitButton = document.querySelector('#signout-button');
+
+function userExists (userList, username) {
+	for (let user of userList) {
+		if (user.username === username) {
+			return true;
+		}
+	}
+	return false;
+}
+
+
+
+// check to see if anything is in storage
+
+function setupAuthenticationButtons() {
+	let userID = sessionStorage.getItem('activeUser');
+
+	if (!userID) {
+		// user is not signed in, so set buttons accordingly
+		authenticationSubmitButton.disabled = false;
+		createAccountSubmitButton.disabled = false;
+		signoutSubmitButton.disabled = true;
+	} else {
+		authenticationSubmitButton.disabled = true;
+		createAccountSubmitButton.disabled = true;
+		signoutSubmitButton.disabled = false;
+	}
+}
+
+setupAuthenticationButtons();
+
+// events
+
+authenticationSubmitButton.addEventListener('click', e => {
+    const usernameInput = document.querySelector("#username");
+    const passwordInput = document.querySelector("#password")
+    authenticationAPI.signInUser(usernameInput.value, passwordInput.value);
+    
+});
+
+signoutSubmitButton.addEventListener('click', e => {
+	sessionStorage.removeItem('activeUser');
+	setupAuthenticationButtons();
+});
+
+createAccountSubmitButton.addEventListener('click', e => {
+	const username = document.querySelector("#createUsername").value;
+    const password = document.querySelector("#createPassword").value;
+    const email = document.querySelector("#email").value;
+
+    // get all the users
+    api.getAllUsers()
+    	.then(allUsers => {
+    		// loop through and see if a user exists already
+    		
+
+
+    		// create the user
+    		api.createUser(username, email, password)
+    			.then(createdUser => {
+    				// save user to localstorage
+    				sessionStorage.setItem("activeUser", createdUser.id);
+    			})
+
+    	})
+
+
+
+    // if the user exists, display an error message, exit
+
+
+    authenticationAPI.createNewUser(newUsernameInput, emailInput, newUserPasswordInput)
+});
 
